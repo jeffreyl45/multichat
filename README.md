1257a90af09ca98f405f223c22a7fc9e2c799287
Jeffrey Lu <jefflu288@gmail.com>
2024-08-07 20:01:33 -0400

    create flask server

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # multichat
 Multi User Chat
+
+# Setup
+1. Setup virtual environment for python: python -m venv server/venvs
+2. Activate virtual environment: source server/venv/bin/activate
+3. Install flask: pip install flask
---
+++ server/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello_world():
+    return "<p> Hello, World!</p>"
+
+@app.route("/sendMessage", methods = ['POST'])
+def send_a_message():
+    message = request.json['message']
+    print(message)
+    return "Success \n", 200
+    
