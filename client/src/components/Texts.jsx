9a28ae3e0edb73580de9ea2727ed4a33b1b5a286
Jeffrey Lu <jefflu288@gmail.com>
2024-08-16 21:30:04 -0400

    render profile photo in messages

--- client/src/components/Texts.jsx
+++ client/src/components/Texts.jsx
@@ -3,14 +3,13 @@ import {useState, useEffect} from 'react';
 import {getWebSocket} from 'util/WebSocket';
 import './Texts.css';
 const Texts = () => {
-    const [texts, setTexts] = useState([]);
-
+    const [datas, setDatas] = useState([]);
     const webSocket = getWebSocket();
     webSocket.off('new text');
     webSocket.on('new text', data => {
         console.log('Got data', data);
-        console.log('texts', texts);
-        setTexts([...texts, data.message]);
+        console.log('texts', datas);
+        setDatas([...datas, data]);
     });
 
     return (
@@ -19,8 +18,14 @@ const Texts = () => {
             <br/>
         <ul className="text-container">
             {
-                texts.map((text, index) => {
-                    return <li key={index} className="Texts">{text}</li>;
+                datas.map((data, index) => {
+                    return <li key={index} className="Texts">
+                        <div>
+                        <img className = "pfp" src = {data.pfp} alt = " user's profile picture"></img>
+                        {data.message}
+                        </div>
+                        
+                        </li>;
                 })
             }
         </ul>
